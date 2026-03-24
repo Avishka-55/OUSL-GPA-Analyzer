8674cb5106004e74ab85c59fee0a99c96f87bf5b
Avishka Piyumantha <139835872+Avishka-55@users.noreply.github.com>
2026-03-24 14:17:23 +0000

    refactor: update Vite configuration for repository base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // IMPORTANT: if your repo is https://github.com/Avishka-55/<REPO_NAME>
+  // then base must be "/<REPO_NAME>/"
+  base: "/OUSL-GPA-Analyzer/",
+});
